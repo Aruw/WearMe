f7bdc9fd6dac6b1976c460d624182525c673d839
André Wada <andre_wada@hotmail.com>
2022-07-09 17:21:31 -0300

    Created project structure.

---
+++ WearMe/src/main/java/br/com/aruw/WearMe/WearMeApplication.java
@@ -0,0 +1,13 @@
+package br.com.aruw.WearMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearMeApplication.class, args);
+	}
+
+}
---
+++ WearMe/src/test/java/br/com/aruw/WearMe/WearMeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aruw.WearMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
