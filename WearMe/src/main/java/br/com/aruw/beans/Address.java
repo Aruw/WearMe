f2e375298e782cd9d4be2e7861600daa480a7cbf
André Wada <andre_wada@hotmail.com>
2022-07-17 01:56:39 -0300

    Updating project main files and correcting bugs.

--- WearMe/src/main/java/br/com/aruw/beans/Address.java
+++ WearMe/src/main/java/br/com/aruw/beans/Address.java
@@ -33,8 +33,8 @@ public class Address {
     @Column(name="REFERENCE_POINT", length=200 ,nullable=false)
     private String referencePoint;
 
-    @ManyToOne
-    @JsonIgnoreProperties("addresses")
+    @OneToOne(mappedBy="address")
+    //@JsonIgnoreProperties("address")
     private User user;
 
     public Address(){
--- WearMe/src/main/java/br/com/aruw/controller/OrderController.java
+++ WearMe/src/main/java/br/com/aruw/controller/OrderController.java
@@ -1,8 +1,10 @@
 package br.com.aruw.controller;
 
+import java.util.List;
+import br.com.aruw.beans.Order;
 import br.com.aruw.dao.OrderDAO;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
@@ -11,5 +13,34 @@ public class OrderController {
 
     @Autowired
     private OrderDAO orderDAO;
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<Order>> getAll(){
+        List<Order> orders = (List<Order>) orderDAO.findAll();
+        if(orders.size() == 0){
+            return ResponseEntity.status(404).build();
+        }
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/order/{cod}")
+    public ResponseEntity<Order> getOrder(@PathVariable int cod){
+        Order order = orderDAO.findById(cod).orElse(null);
+        if(order == null){
+            return ResponseEntity.status(404).build();
+        }
+        return ResponseEntity.ok(order);
+    }
+
+    @PostMapping("/neworder")
+    public ResponseEntity<Order> addOrder(@RequestBody Order object){
+        try{
+            orderDAO.save(object);
+            return ResponseEntity.ok(object);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(403).build();
+        }
+    }
 
 }
